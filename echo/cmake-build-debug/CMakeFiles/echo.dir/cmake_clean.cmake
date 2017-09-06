21754999bfe98eb7beb6d77e5d675be3dab612ac
Yuan-T-Xuan <tonyxuan26@foxmail.com>
2017-09-06 19:58:15 -0400

    begin to work on echo server

---
+++ echo/cmake-build-debug/CMakeFiles/echo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/echo.dir/main.c.o"
+  "CMakeFiles/echo.dir/test.c.o"
+  "echo.pdb"
+  "echo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/echo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
