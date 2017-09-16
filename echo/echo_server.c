8c639fd8e2c0a21da3ca9d167d687eeebb249cd7
Pod5GS <wcincredible@live.com>
2017-09-15 22:55:19 -0700

    add logging module

--- echo/echo_server.c
+++ echo/echo_server.c
@@ -4,8 +4,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include "logging.h"
 
-const unsigned short PORT = 8089;
+const unsigned short PORT = 8081;
 const int BUFFER_SIZE = 40960;
 
 int make_socket(unsigned short port)
@@ -15,7 +17,10 @@ int make_socket(unsigned short port)
 
     sock = socket(PF_INET, SOCK_STREAM, 0);
     if(sock < 0) {
-        perror("failed to establish socket");
+        set_file();
+        perror("failed to establish socket\n");
+        log_error("failed to establish socket\n");
+        close_file();
         exit(EXIT_FAILURE);
     }
 
@@ -23,7 +28,10 @@ int make_socket(unsigned short port)
     name.sin_port = htons(port);
     name.sin_addr.s_addr = htonl(INADDR_ANY);
     if(bind(sock, (struct sockaddr *) &name, sizeof(name)) < 0) {
-        perror("failed to bind a name to socket");
+        set_file();
+        perror("failed to bind a name to socket\n");
+        log_error("failed to bind a name to socket\n");
+        close_file();
         exit(EXIT_FAILURE);
     }
 
@@ -35,7 +43,10 @@ int read_from_client(int filedes, char *buffer) {
 
     nbytes = (int) read(filedes, buffer, BUFFER_SIZE);
     if(nbytes < 0) {
-        perror("something went wrong when reading FD");
+        set_file();
+        perror("something went wrong when reading FD\n");
+        log_error("something went wrong when reading FD\n");
+        close_file();
         exit(EXIT_FAILURE);
     }
     else if(nbytes == 0) {
@@ -52,7 +63,10 @@ int main() {
     fd_set active_fd_set, read_fd_set;
     sock = make_socket(PORT);
     if(listen(sock, SOMAXCONN) < 0) {
-        perror("failed to listen on the socket");
+        set_file();
+        perror("failed to listen on the socket\n");
+        log_error("failed to listen on the socket\n");
+        close_file();
         exit(EXIT_FAILURE);
     }
     FD_ZERO(&active_fd_set);
@@ -64,7 +78,10 @@ int main() {
         /* Block until input arrives on one or more active sockets. */
         read_fd_set = active_fd_set;
         if(select(FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0) {
-            perror("failed when processing \"select\"");
+            set_file();
+            perror("failed when processing \"select\"\n");
+            log_error("failed when processing \"select\"\n");
+            close_file();
             exit(EXIT_FAILURE);
         }
 
@@ -74,10 +91,16 @@ int main() {
                     size = sizeof(clientname);
                     new = accept(sock, (struct sockaddr *) &clientname, (socklen_t *) &size);
                     if(new < 0) {
-                        perror("failed to accept new connection");
+                        set_file();
+                        perror("failed to accept new connection\n");
+                        log_error("failed to accept new connection\n");
+                        close_file();
                         exit(EXIT_FAILURE);
                     }
-                    printf("Server: new connection established from a host.\n");
+                    char *ip = inet_ntoa((&clientname)->sin_addr);
+                    set_file();
+                    log_info("Server: new connection established from client, client ip is %s.\n", ip);
+                    close_file();
                     FD_SET(new, &active_fd_set);
                 } else {
                     /* Data arriving on an already-connected socket. */
@@ -88,10 +111,17 @@ int main() {
                         FD_CLR(i, &active_fd_set);
                     } else {
                         if(send(i, buffer, (size_t) nbytes, 0) != nbytes) {
-                            perror("failed to send message back");
+                            char *ip = inet_ntoa((&clientname)->sin_addr);
+                            set_file();
+                            perror("failed to send message back to client\n");
+                            log_error("failed to send message back to client, client ip is %s\n", ip);
+                            close_file();
                             exit(EXIT_FAILURE);
                         }
-                        printf("%s", buffer);
+                        char *ip = inet_ntoa((&clientname)->sin_addr);
+                        set_file();
+                        log_info("New message from client ip: %s, message is: %s", ip, buffer);
+                        close_file();
                     }
                 }
             }
